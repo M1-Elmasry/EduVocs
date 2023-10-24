9f963d9e2c7383d738e204c06773c5529542f43f
M1-Elmasry <mostafamfelmasry@outlook.com>
2023-10-25 02:31:02 +0300

    add manager module

---
+++ manager.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""module represents manager class"""
+
+
+class Manager:
+
+    @classmethod
+    def translate(cls, text, lang_from, lang_to):
+        """test"""
+        return text[::-1]
