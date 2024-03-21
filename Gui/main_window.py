accf59573a34e8ce6f65a3996c446c784434561c
M1-Elmasry <mostafamfelmasry@outlook.com>
2024-03-21 04:06:57 +0200

    write user's data into database

--- Gui/main_window.py
+++ Gui/main_window.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 
 from PySide6.QtWidgets import (
-                        QWidget,
-                        QMainWindow,
-                        QLabel,
-                        QLineEdit,
-                        QPushButton,
-                        QComboBox,
-                        QTextEdit,
-                        QApplication,
-                        QVBoxLayout,
-                        QHBoxLayout
-                            )
-from .user_window import UserWindow
+    QWidget,
+    QMainWindow,
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QComboBox,
+    QTextEdit,
+    QVBoxLayout,
+    QHBoxLayout,
+    QCompleter,
+)
 from PySide6.QtCore import Qt
-from PySide6.QtGui import QIcon
 from .center_widget import center
-from .knowing_window import KnowingWindow
-# Insure about he email format
+from ..storage import storage
+from ..translate import supported_languages
 from re import match
+from .learning_window import LearningWindow
 
 
 # Creating Main_Window_Class
@@ -29,8 +28,8 @@ class MainWindow(QMainWindow):
         # Big screens
         self.setMinimumSize(900, 600)
         self.setWindowTitle("EduVocs")
-        self.setWindowIcon(QIcon("/home/mahmoud/Desktop/Repos/EduVocs/Gui/assets/EduVocs_icon.png"))
-        
+        self.dict_languages = supported_languages
+
         center(self)
         if self.size().width() <= 767:
             # Create a main widjet to hold the main layout
@@ -54,11 +53,11 @@ class MainWindow(QMainWindow):
             info_layout = QVBoxLayout()
             info_layout.setAlignment(Qt.AlignHCenter)
 
-            user_name = self.UserName()
-            user_email = self.UserEmail()
+            self.user_name = self.UserName()
+            self.user_email = self.UserEmail()
 
-            info_layout.addWidget(user_name)
-            info_layout.addWidget(user_email)
+            info_layout.addWidget(self.user_name)
+            info_layout.addWidget(self.user_email)
 
             main_layout.addLayout(info_layout)
 
@@ -66,14 +65,13 @@ class MainWindow(QMainWindow):
             lang_layout = QVBoxLayout()
             lang_layout.setAlignment(Qt.AlignHCenter)
 
-            native_lang = self.NativeLang()
-            target_lang = self.TargetLang()
+            self.native_lang = self.NativeLang()
+            self.target_lang = self.TargetLang()
 
-            lang_layout.addWidget(native_lang)
-            lang_layout.addWidget(target_lang)
+            lang_layout.addWidget(self.native_lang)
+            lang_layout.addWidget(self.target_lang)
 
             main_layout.addLayout(lang_layout)
-
 
             # Adding the Done button
             button_layout = QVBoxLayout()
@@ -91,9 +89,7 @@ class MainWindow(QMainWindow):
             alert_box = self.alert_box()
             alert_box_layout.addWidget(alert_box)
 
-
             main_layout.addLayout(alert_box_layout)
-
 
             self.welcome_header = welcome_header
             self.welcome_landing = welcome_landing
@@ -150,7 +146,6 @@ class MainWindow(QMainWindow):
 
             main_layout.addLayout(lang_layout)
 
-
             # Adding the Done button
             button_layout = QHBoxLayout()
             done = self.done_button()
@@ -165,9 +160,7 @@ class MainWindow(QMainWindow):
             alert_box = self.alert_box()
             alert_box_layout.addWidget(alert_box)
 
-
             main_layout.addLayout(alert_box_layout)
-
 
             self.welcome_header = welcome_header
             self.welcome_landing = welcome_landing
@@ -180,22 +173,21 @@ class MainWindow(QMainWindow):
 
             central_widget.setLayout(main_layout)
 
-
-
     def welcome_header(self):
         # Make a welcome message for the user at application intrance
         heading = QLabel("Welcome")
         heading.setAlignment(Qt.AlignHCenter)
         heading.setStyleSheet("font-size: 40px; margin-top: 40px")
-        return (heading)
+        return heading
 
     def welcome_landing(self):
-        WelcomeMessage = QLabel("Welcome to EduVocs, Where Language mestery meets innovation")
+        WelcomeMessage = QLabel(
+            "Welcome to EduVocs, Where Language mestery meets innovation"
+        )
         WelcomeMessage.setAlignment(Qt.AlignHCenter)
         WelcomeMessage.setStyleSheet("font-size: 20px")
         WelcomeMessage.setContentsMargins(0, 0, 0, 0)
-        return(WelcomeMessage)
-
+        return WelcomeMessage
 
     # Input the name of the user
     def UserName(self):
@@ -204,7 +196,7 @@ class MainWindow(QMainWindow):
         self.user_name.setAlignment(Qt.AlignCenter)  # Align placeholder text to center
         self.user_name.setFixedSize(300, 40)
         self.user_name.setFocusPolicy(Qt.ClickFocus)
-        return (self.user_name)
+        return self.user_name
 
     # Input the email of the user
     def UserEmail(self):
@@ -213,40 +205,43 @@ class MainWindow(QMainWindow):
         self.user_email.setFixedSize(300, 40)
         self.user_email.setPlaceholderText("Your email")
         self.user_email.setFocusPolicy(Qt.ClickFocus)
-        return (self.user_email)
+        return self.user_email
 
     # Create a native language selection box
     def NativeLang(self):
         self.native_language = QComboBox(self)
         self.native_language.setFixedSize(300, 40)
-        # Add items to the combo box
+        self.native_language.setEditable(True)
         self.native_language.addItem("Your Native Language")
-        self.native_language.addItem("Arabic")
-        self.native_language.addItem("English")
-        self.native_language.addItem("French")
-        self.native_language.addItem("German")
         self.native_language.setCurrentIndex(0)
 
-        return (self.native_language)
+        for lang in self.dict_languages.keys():
+            self.native_language.addItem(lang)
+
+        completer = QCompleter(self.dict_languages.keys())
+        self.native_language.setCompleter(completer)
+
+        return self.native_language
 
     # Create a target language selection box
     def TargetLang(self):
         self.target_language = QComboBox(self)
         self.target_language.setFixedSize(300, 40)
-
-        # Add items to the combo box
+        self.target_language.setEditable(True)
         self.target_language.addItem("Your Target Language")
-        self.target_language.addItem("Arabic")
-        self.target_language.addItem("English")
-        self.target_language.addItem("French")
-        self.target_language.addItem("German")
         self.target_language.setCurrentIndex(0)
-        return (self.target_language)
+
+        for lang in self.dict_languages.keys():
+            self.target_language.addItem(lang.lower())
+
+        completer = QCompleter(self.dict_languages.keys())
+        self.target_language.setCompleter(completer)
+        return self.target_language
 
     def alert_element(self):
         self.message_box_label = QLabel("Alerts:", self)
         self.message_box_label.setAlignment(Qt.AlignHCenter)
-        return(self.message_box_label)
+        return self.message_box_label
 
     def alert_box(self):
         self.message_box = QTextEdit(self)
@@ -254,7 +249,7 @@ class MainWindow(QMainWindow):
         self.message_box.setFixedWidth(600)
         self.message_box.setStyleSheet("margin-bottom: 50px")
         self.message_box.setAlignment(Qt.AlignCenter)  # Align text to center
-        return(self.message_box)
+        return self.message_box
 
     def done_button(self):
         # Make the done buttom
@@ -264,19 +259,21 @@ class MainWindow(QMainWindow):
         self.button.setFocus()
         """ When the Done buttom clicked, it will turn to the translation screen """
         self.button.clicked.connect(self.click_done_button)
-        return (self.button)
+        return self.button
 
     def click_done_button(self):
         user_name = self.user_name.text()
         user_email = self.user_email.text()
-        email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        name_pattern = r'^[a-zA-Z]+$'
+        email_pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        name_pattern = r"^[a-zA-Z]+$"
 
         if not match(name_pattern, user_name):
             self.message_box.setText("Invalid name. Please enter a valid name.")
 
         elif not match(email_pattern, user_email):
-            self.message_box.setText("Invalid email format. Please enter a valid email.")
+            self.message_box.setText(
+                "Invalid email format. Please enter a valid email."
+            )
 
         elif self.native_language.currentIndex() == 0:
             self.message_box.setText("Please choose your native language")
@@ -286,13 +283,18 @@ class MainWindow(QMainWindow):
 
         else:
             self.message_box.setText("everything is good")
-            new_window = UserWindow()
-            self.hide()
-            new_window.show()
-
+            storage.save_user_data(
+                self.user_name.text(),
+                self.user_email.text(),
+                self.dict_languages[self.native_language.currentText()],
+                self.dict_languages[self.target_language.currentText()],
+            )
+            next_window = LearningWindow()
+            self.close()
+            next_window.show()
 
     def window_shown(self, event):
-    # Set the stretch factors for the left and right widgets
+        # Set the stretch factors for the left and right widgets
         main_layout = self.centralWidget().layout()
 
         main_layout.setStretchFactor(self.welcome_header, 1)
